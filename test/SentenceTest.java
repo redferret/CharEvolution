b8156a197697539d5eb313386ac79d7e03fd83b0
Richard <rdesilvey@gmail.com>
2017-05-30 20:39:55 -0600

    Added two tests

---
+++ test/SentenceTest.java
@@ -0,0 +1,48 @@
+
+import charevolution.Sentence;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the class Sentence
+ * @author Richard
+ */
+public class SentenceTest {
+    
+    public SentenceTest(){
+        
+    }
+    
+    @Test
+    public void copyAndMutateTest(){
+        
+        Sentence testSentence = new Sentence("This is a Test");
+        Sentence child = testSentence.copyAndMutate();
+        
+        assertFalse("Sentences should not match", 
+                testSentence.toString().equalsIgnoreCase(child.toString()));
+        
+    }
+    
+    @Test
+    public void setFitnessTest(){
+        String testString = "This is a Test";
+        Integer testLength = testString.length();
+        
+        Sentence testTarget = new Sentence(testString);
+        Sentence perfectMatch = new Sentence(testString);
+        
+        Sentence testMatch = new Sentence("This IS fwokit");
+        Integer expectedFitness = 7;
+        Integer testMatchFitness = testMatch.getFitness();
+        
+        testMatch.setFitness(testTarget);
+        assertEquals("Fitness is not correct", expectedFitness, testMatchFitness);
+        
+        perfectMatch.setFitness(testTarget);
+        testMatchFitness = perfectMatch.getFitness();
+        assertEquals("Fitness is not a perfect match", testMatchFitness, testLength);
+    }
+    
+}
